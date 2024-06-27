0f3c3f09b61776a8dbfb351edcf0f775a00e9414
jaro3 <23013892-jaro3@users.noreply.replit.com>
2024-06-27 15:25:23 +0000

    Implement submit

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,52 @@ import { useState } from "react";
 export default function App() {
   const EMPTY = "";
   const MAX_LENGHT = 2;
+  
+  const STATE_THINKING = 1;
+  const STATE_ANSWERED = 2;
+
+  const RESULT_NOT_DEFINED = 0;
+  const RESULT_CORRECT = 1;
+  const RESULT_INCORRECT = 2;
+
   const [value, setValue] = useState(EMPTY);
+  const [state, setState] = useState(STATE_THINKING);
+  const [result, setResult] = useState(RESULT_NOT_DEFINED);
 
   function onAddDigit(digit) {
+    if (state != STATE_THINKING)
+      return
+    
     if (value.length < MAX_LENGHT) {
       setValue(value + digit);
+    } // TODO else show message
+  }
+
+  function onSubmit() {
+    setState(STATE_ANSWERED)
+    if (value == 34) {
+      setResult(RESULT_CORRECT)
+
+      setTimeout(() => {
+          setState(STATE_THINKING)
+          setValue(EMPTY)
+          setResult(RESULT_NOT_DEFINED)
+      }, 3000);
+    } else {
+      setResult(RESULT_INCORRECT)
+
+      setTimeout(() => {
+        setState(STATE_THINKING)
+        setValue(EMPTY)
+        setResult(RESULT_NOT_DEFINED)
+      }, 3000);
     }
   }
 
-  function onSubmit() {}
-
   function onDelete() {
+    if (state != STATE_THINKING)
+      return
+
     setValue(EMPTY);
   }
 
@@ -24,7 +59,7 @@ export default function App() {
     <main>
       <div id="zadani">
         <span id="operand1">12</span>
-        <span id="operator1">+</span>
+        <span id="operator">+</span>
         <span id="neznama">
           <span id="operand2">{value}</span>
         </span>
@@ -49,6 +84,9 @@ export default function App() {
       </div>
 
       {/* <ButtonMenu onShowMenu={onShowMenu} /> */}
+
+      <IconCorrect isVisible={result == RESULT_CORRECT} />
+      <IconIncorrect isVisible={result == RESULT_INCORRECT} />
     </main>
   );
 }
@@ -97,5 +135,27 @@ function ButtonMenu({ onShowMenu }) {
       onClick={onShowMenu}
       src="images/menu_24dp_FILL0_wght400_GRAD0_opsz24.svg"
     ></img>
+  );
+}
+
+function IconCorrect({ isVisible }) {
+  return isVisible ? (
+    <img
+      id="correct"
+      src="images/check_24dp_FILL0_wght400_GRAD0_opsz24.svg"
+    ></img>
+  ) : (
+    <p></p>
+  );
+}
+
+function IconIncorrect({ isVisible }) {
+  return isVisible ? (
+    <img
+      id="incorrect"
+      src="images/close_24dp_FILL0_wght400_GRAD0_opsz24.svg"
+    ></img>
+  ) : (
+    <p></p>
   );
 }
